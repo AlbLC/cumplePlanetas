7e33a7bf7d00e076f45f14b8f373c3587913cf51
Lydia Vega <lyvegama@gmail.com>
2022-10-17 09:33:49 +0200

    componentes y primeros estilos

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,25 @@
-
-// import './App.css';
+import React from "react";
+// import { BrowserRouter } from "react-router-dom";
+import "./App.css";
+import Calculadora from "./components/Calculadora";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Info from "./components/Info";
+import Carrusel from "./components/Carrusel";
+import Navbar from "./components/Navbar";
+import MenuPlanetario from "./components/MenuPlanetario";
+import Tarjetas from "./components/Tarjetas"
 
 function App() {
+
   return (
-    <div className="App">
+    <div className="app">
+      {/* <Navbar /> */}
+      <Carrusel />
+      <MenuPlanetario/>
+      <Info />
+      <Calculadora />
+      <Tarjetas/>
     </div>
   );
 }
-
 export default App;
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import imgPrueba from '../imgPrueba.jpg'
+
+function Navbar() {
+
+  return (
+    <div className="navbar">
+    
+    </div>
+  );
+
+}
+
+export default Navbar;
