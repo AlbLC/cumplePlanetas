745e453b278330ceaf684f7e01573e7c3918e48a
Alberto L <71764455+AlbLC@users.noreply.github.com>
2022-10-19 09:49:56 +0200

    /Home

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Routes, Route } from "react-router-dom"
 import Calculadora from './Calculadora'
+import Home from './Home'
 
 
 const Main = () => {
@@ -10,6 +11,8 @@ const Main = () => {
       <Routes>
 
         <Route path='/planets' element={<Calculadora />} />
+        <Route path='/' element={<Home />} />
+        
 
       </Routes>
     </div>
--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
 const planet = require("../controllers/planet.controllers");
 // const users = require("../controllers/users.controllers");
+const home = require("../controllers/home.controllers")
 
 router.get("/planets", planet.getPlanets);
 // router.get("/users", users.setUsers);
+router.post("/home",home.getHome)
 
 
 module.exports = router;
